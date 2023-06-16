0683443688695019df69ab65ecfee0fcfb0d8fc3
Gustavo Akyama <gustavoakyamamga01@gmail.com>
2023-06-16 11:30:20 -0300

    News Update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,8 +11,7 @@ class WelcomeController < ApplicationController
       lang = "en"
     end
 
-    url = "https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=#{api_key}"
-    url = "https://newsapi.org/v2/everything?q=cryptocurrency&language=#{lang}&apiKey=#{api_key}"
+    url = "https://newsapi.org/v2/everything?q=crypto&language=#{lang}&apiKey=#{api_key}"
     response = HTTParty.get(url)
     
     @news = response.parsed_response['articles'].first(3)
